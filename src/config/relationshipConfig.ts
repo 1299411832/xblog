51cc6aa95ad38ae9f2b1baf6f4eec83e341675d2
迷体星球 <126231149+1299411832@users.noreply.github.com>
2026-09-02 20:09:48 +0800

    Update relationshipConfig.ts

--- src/config/relationshipConfig.ts
+++ src/config/relationshipConfig.ts
@@ -2,13 +2,13 @@ import type { RelationshipConfig } from "../types/config";
 
 export const relationshipConfig: RelationshipConfig = {
 	// 恋爱开始日期
-	startDate: "2026-01-06",
+	startDate: "2012-05-06",
 	// 双方名称
-	name1: "---TSH",
-	name2: "CXY---",
+	name1: "---XZJ",
+	name2: "guitar---",
 	// 双方头像
-	avatar1: "/assets/ziyuan/tsh.jpg",
-	avatar2: "/assets/ziyuan/cxy.jpg",
+	avatar1: "/assets/ziyuan/3.jpg",
+	avatar2: "/assets/ziyuan/g.jpeg",
 	// 小组件标题
-	title: "我和宝宝在一起已经",
+	title: "我从宇宙大爆炸开始练琴",
 };
